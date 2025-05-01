8d059e0797657d9716ff362ba90e09f92a464b66
itsumray <raykawano675@gmail.com>
2025-05-01 19:02:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 let nextPageToken = '';
 let query = '';
 let shownCount = 0;
+let mode = 'search'; // or 'recommend'
 
 function searchYouTube() {
   query = document.getElementById('search-input').value;
+  mode = 'search';
   shownCount = 0;
   nextPageToken = '';
   document.getElementById('video-results').innerHTML = '';
-  document.getElementById('popup').classList.remove('hidden');
+  document.getElementById('modal').classList.remove('hidden');
+  fetchVideos();
+}
+
+function getRecommended() {
+  mode = 'recommend';
+  shownCount = 0;
+  nextPageToken = '';
+  document.getElementById('video-results').innerHTML = '';
+  document.getElementById('modal').classList.remove('hidden');
   fetchVideos();
 }
 
@@ -15,10 +26,20 @@ function loadMore() {
   fetchVideos();
 }
 
+function closeModal() {
+  document.getElementById('modal').classList.add('hidden');
+}
+
 function fetchVideos() {
   const apiKey = 'AIzaSyC_O58TFr5rI-UibHnTS-oZoRzNGONQGAw';
   const maxResults = 10;
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${encodeURIComponent(query)}&key=${apiKey}&maxResults=${maxResults}&pageToken=${nextPageToken}`;
+
+  let url = '';
+  if (mode === 'search') {
+    url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${encodeURIComponent(query)}&key=${apiKey}&maxResults=${maxResults}&pageToken=${nextPageToken}`;
+  } else if (mode === 'recommend') {
+    url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=${maxResults}&pageToken=${nextPageToken}`;
+  }
 
   fetch(url)
     .then(res => res.json())
@@ -28,7 +49,7 @@ function fetchVideos() {
       shownCount += 4;
 
       results.forEach(item => {
-        const videoId = item.id.videoId;
+        const videoId = item.id.videoId || item.id;
         const iframe = document.createElement('iframe');
         iframe.src = `https://www.youtube.com/embed/${videoId}`;
         iframe.allowFullscreen = true;
